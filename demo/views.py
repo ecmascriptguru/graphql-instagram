5e8dfa91354feef1fa086fe9571ad2a0d532a6b9
Alexis Richard <ecmascript.guru@gmail.com>
2018-11-21 10:06:34 +0200

    demo app created.

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from .models import InstagramPhoto
 
 # Create your views here.
+
+class DemoListView(ListView):
+    template_name = 'demo/demo_list_view.html'
+    model = InstagramPhoto
